8cb9f6cb73f2735fd536ddbdc1155d15e2c62610
whalemagic <chesskit35@gmail.com>
2025-04-07 00:27:20 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', e => {
+  console.log('Service Worker: Installed');
+});
+
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
